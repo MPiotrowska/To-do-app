7965f9134a59e741ff11f045840e925eef5a540d
MPiotrowska <monikapiotrowska27@gmail.com>
2020-02-19 14:29:54 +0000

    add js logic

--- scripts.js
+++ scripts.js
@@ -7,10 +7,21 @@ var input = document.getElementById('input');
 var button = document.getElementById('button');
 
 
+
+
 //functions
 
-function inputChange(event) {
-    console.log(event)
+function addTodos() {
+    // save user input
+    var userInput = event.target.value;
+    // add html with user input
+    var listItem = `<li>${userInput}</li>`;
+    // append to the page
+    todos.insertAdjacentHTML('beforeend', listItem);
+    //clear input so user doens't need to do it
+    event.target.value = ''
 }
 
-input.addEventListener('change', inputChange)
+
+// Listen for change event
+input.addEventListener('change', addTodos);
